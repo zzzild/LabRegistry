59424a6d4c80d0476ad848418ac3b606f150c0e0
zild <bilaldi393@gmail.com>
2024-02-11 21:30:31 +0700

    membuat fungsi pada tabel move dan penambahan 1 database

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -24,5 +24,28 @@ router.delete("/users/:userId", (req, res) => {
     });
 });
 
+router.post("/move-to-lulus/:userId", (req, res) => {
+    const userId = req.params.userId;
+    const moveToLulusSql = "INSERT INTO dataditerima SELECT * FROM datapendaftar WHERE id = ?";
+    const deleteFromPendaftarSql = "DELETE FROM datapendaftar WHERE id = ?";
+    
+    db.query(moveToLulusSql, userId, (err, result) => {
+        if (err) {
+            console.error('Error:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            // Hapus baris pengguna dari tabel pendaftar setelah dipindahkan
+            db.query(deleteFromPendaftarSql, userId, (deleteErr, deleteResult) => {
+                if (deleteErr) {
+                    console.error('Error:', deleteErr);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.status(200).send('User moved to "diterima" table successfully');
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
 
